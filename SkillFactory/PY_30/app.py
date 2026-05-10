1df2c3187d017953a6c3170fc08c89ee8b396e9d
remezovmd <remezovmd@gmail.com>
2026-05-10 10:12:52 +1000

    Кредитный калькулятор streamlit

--- SkillFactory/PY_30/app.py
+++ SkillFactory/PY_30/app.py
@@ -4,7 +4,7 @@ import datetime
 from dateutil.relativedelta import relativedelta
 
 def calculate(sum: int, rate: float, loan_term: int, type: str, date: datetime) -> list:
-    """
+    '''
     Расчет графика платежей
     
     Параметры:
@@ -15,7 +15,7 @@ def calculate(sum: int, rate: float, loan_term: int, type: str, date: datetime) 
     date - дата первого платежа (datetime или None)
     
     Возвращает список [Ежемесячный платеж, Общая сумму займа, DataFrame с графиком платежей]
-    """
+    '''
     payment_schedule = []
     p = rate / 12 / 100
     balance = sum
@@ -33,13 +33,13 @@ def calculate(sum: int, rate: float, loan_term: int, type: str, date: datetime) 
             payment = debt_portion + interest_portion
             
         payment_schedule_str = {}
-        payment_schedule_str["Дата платежа"] = date + relativedelta(months=month) 
-        payment_schedule_str["Остаток долга"] = round(balance, 2)
-        payment_schedule_str["Платеж"] = round(payment, 2)
-        payment_schedule_str["Процентная часть"] = round(interest_portion, 2)
-        payment_schedule_str["Долговая часть"] = round(debt_portion, 2)
+        payment_schedule_str['Дата платежа'] = date + relativedelta(months=month) 
+        payment_schedule_str['Остаток долга'] = round(balance, 2)
+        payment_schedule_str['Платеж'] = round(payment, 2)
+        payment_schedule_str['Процентная часть'] = round(interest_portion, 2)
+        payment_schedule_str['Долговая часть'] = round(debt_portion, 2)
         balance -= debt_portion 
-        payment_schedule_str["Долг на конец месяца"] = max(0, round(balance, 2))
+        payment_schedule_str['Долг на конец месяца'] = max(0, round(balance, 2))
         payment_schedule.append(payment_schedule_str)
 
     payment_schedule[-1]['Долговая часть'] = round(payment_schedule[-1]['Долговая часть'] + payment_schedule[-1]['Долг на конец месяца'], 2)
@@ -47,13 +47,12 @@ def calculate(sum: int, rate: float, loan_term: int, type: str, date: datetime) 
     payment_schedule[-1]['Долг на конец месяца'] = 0
 
     payment_schedule_table = pd.DataFrame(payment_schedule)
-    all_sum = payment_schedule_table["Платеж"].sum()
+    all_sum = payment_schedule_table['Платеж'].sum()
     
     return [payment, all_sum, payment_schedule_table]
 
 st.title('Кредитный калькулятор')
-col1, col2 = st.columns(2, gap="medium")
-col3 = st.container()
+col1, col2 = st.columns(2, gap='medium')
 with col1:
     sum = st.number_input('Сумма кредита', min_value=10000, max_value=100000000, step=1000, value=10000)
     rate = st.number_input('Ставка', min_value=0.0, max_value=50.0, step=1.0, value=20.0)
@@ -63,7 +62,7 @@ with col1:
     loan_term_month = int(loan_term_month[0]) if loan_term_month[1] == 'месяц' else int(loan_term_month[0]) * 12
     
     type = st.radio('Тип платежа', ['аннуитетный', 'дифференциальный'], horizontal=True)
-    date = st.date_input("Дата первого платежа", format = "DD.MM.YYYY", value = datetime.date.today(), min_value = datetime.date.today())
+    date = st.date_input('Дата первого платежа', format = 'DD.MM.YYYY', value = datetime.date.today(), min_value = datetime.date.today())
     run = st.button('Рассчитать', type='primary')
 
 if run:
@@ -81,5 +80,5 @@ if run:
             st.text(f'{sum:,.2f}'.replace(',', ' '))
             st.text(f'{result[1]:,.2f}'.replace(',', ' '))
             st.text(f'{result[1] - sum:,.2f}'.replace(',', ' '))
-    with st.expander("Показать график платежей"):        
+    with st.expander('Показать график платежей'):        
         st.dataframe(result[2], hide_index=True)
